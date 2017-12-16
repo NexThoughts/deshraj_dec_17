b1d587ac34406bc7fee2cb0cb16fe35ce4edb0e5
Sakshi <khatter.sakshi@gmail.com>
2017-12-16 15:14:07 +0530

    domain class

---
+++ src/main/java/com/example/Task.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.util.Date;
+
+public class Task {
+
+    String taskName;
+    String description;
+    String taskId;
+    String assigneeId;
+
+    Enum.taskStatus taskStatus;
+
+    Date dueDate;
+
+
+}
